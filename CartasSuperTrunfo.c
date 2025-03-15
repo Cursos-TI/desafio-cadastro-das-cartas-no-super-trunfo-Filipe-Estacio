59337fe95fd5dbff11f4a733f7f2a39bb0243b1a
Filipe-Estacio <filipemaop@hotmail.com>
2025-03-15 20:37:42 +0000

    Iniciado desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,8 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+
+
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -17,6 +19,15 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    char estado[50];
+    char codigo[50];
+    char nomedacidade[50];
+    int população;
+    float area;
+    int numerodepontosturisticos;
+    float PIB;
+
 
     return 0;
 }
